6162051f304dc3eaad7f149ed1c392e2f3093e00
Olivier G <zotri@hotmail.com>
2024-12-28 13:35:11 +0100

    wip

--- script.sh
+++ script.sh
@@ -1,25 +1,59 @@
 #!/bin/bash
 
+# ROOT_DIR=$(realpath "$(dirname "$0")")
 NOTES_PATH="$HOME/shared/notes/"
 NOTES_BASENAME=$(basename "$NOTES_PATH")
 
-only_files_cmd=$(find "$NOTES_PATH" ! -name "$NOTES_BASENAME" -type f -printf "%P\n")
-# only_dirs_cmd=$(find "$NOTES_PATH" ! -name "$NOTES_BASENAME" -type d -printf "%P\n")
-# dirs_and_files_cmd=$(find "$NOTES_PATH" ! -name "$NOTES_BASENAME" -type d -printf "%P/\n" -o -type f -printf "%P\n")
+FILES=$(find "$NOTES_PATH" ! -name "$NOTES_BASENAME" -type f -printf "%P\n")
+# DIRS=$(find "$NOTES_PATH" ! -name "$NOTES_BASENAME" -type d -printf "%P\n")
+# DIRS_AND_FILES=$(find "$NOTES_PATH" ! -name "$NOTES_BASENAME" -type d -printf "%P/\n" -o -type f -printf "%P\n")
 
-choice=$(fzf --border=top --border-label="COUCOU" <<< "$only_files_cmd")
+TERM_WIDTH=$(tput cols)
+# TERM_HEIGHT=$(tput lines)
 
-# echo "$choice"
-chosenfile="$NOTES_PATH$choice"
+usage_and_exit() {
+    echo 'Usage:'
+    echo "  $0                  search any note"
+    echo "  $0 find <query>     search for a note with query, opens it if only 1 found, else opens the search with query"
+    echo "                               'search', 'f' and 's' are aliases for find"
+    echo "  $0 <query>          like 'find', for any string that doesn't match any other parameter"
+    exit
+}
 
-term_width=$(tput cols)
-term_height=$(tput lines)
-colwidth=$(("$term_width" / 2 - 10))
-formatted_file=$(fmt -s -w $colwidth < "$chosenfile")
+format_note() {
+    # TODO: interpret markdown (ruby script ?)
+    fmt -s -w $colwidth < "$chosenfile"
+}
+
+find_note() {
+    [ -n "$1" ] && QUERY=$1
+    local choice chosenfile colwidth
+    choice=$(fzf --query "$QUERY" -1 --border=top --border-label="COUCOU" <<< "$FILES")
+
+    # echo "$choice"
+    chosenfile="$NOTES_PATH$choice"
+    # file_date=$(date -r "$chosenfile" "+%Y-%m-%d %H:%M")
+    colwidth=$(("$TERM_WIDTH" / 2 - 4))
+
+    formatted_file=$(format_note)
+    echo "$formatted_file"
+}
+
+case $1 in
+    "") find_note;;
+    find | search | f | s) find_note "$2";;
+    help | -h | --help) usage_and_exit;;
+    *) find_note "$1";;
+esac
 
 
-pr --columns 2 -S" | " -o 2 -w "$term_width" -l "$term_height" -D "%Y-%m-%d %H:%M" <<< "$formatted_file"
+# lines_count=$(wc -l <<< "$formatted_file")
 
-# res=$(pr --columns 2 -S" | " -w "$term_width" -l "$term_height" -D "%Y-%m-%d %H:%M" <<< "$formatted_file")
-# less <<< "$res"
+# echo $lines_count, $TERM_HEIGHT
+
+# if (("$lines_count" > "$TERM_HEIGHT" - 1)); then
+#     pr --columns 2 -w "$TERM_WIDTH" -l "$TERM_HEIGHT" -D "Last modified: $file_date" <<< "$formatted_file"
+# else
+#     echo "$formatted_file"
+# fi
 
--- src/note.rb
+++
@@ -1,4 +0,0 @@
-class Note < ActiveRecord::Base
-  self.table_name = 'notes'
-  self.primary_key = 'id'
-end
