35e6844df350a2015ff21c321983afc773096d4a
Olivier G <zotri@hotmail.com>
2025-01-07 00:41:29 +0100

    remove parser job from list renderer

--- md_renderer_lib/modules/lists.rb
+++ md_renderer_lib/modules/lists.rb
@@ -8,39 +8,28 @@ module Lists
   private
 
   def render_unord_list(str)
-    str = sanitize_list_string(str)
     str = render_inline_chunks(str)
     lines = to_lines_with_style(str, keep_indent: true, word_wrap: true)
 
     prev_line = ""
 
-    lines.map! do |line|
-      line = case line
-      when /(^\s?- )/ then line.gsub($1, "• ")
-      when /^(\s{2,3}- )/ then line.gsub($1, "  ◦ ")
-      when /^(\s{4,})(- )/
-        indent_levels = $1.length / 2
-        indent = "  " * indent_levels
-        line.gsub("#{$1}#{$2}", "#{indent}▪ ")
-      else
-        indent_levels = prev_line.match(/^\s*/)[0].length / 2
-        indent = "  " * (indent_levels + 1)
-        "#{indent}#{line}"
-      end
-      prev_line = line
-      "  #{line}"
-    end.join("\n")
-  end
+    pp lines
 
-  private
-
-  def sanitize_list_string(str)
-    lines = str.strip.squeeze("\n").split("\n")
-    lines.reduce("") do |acc, s|
-      next s if acc.empty?
-
-      stripped = s.strip
-      stripped.match?(/^- /) ? "#{acc}\n#{s}" : "#{acc} #{stripped}"
-    end
+    # lines.map! do |line|
+    #   line = case line
+    #   when /(^\s?- )/ then line.gsub($1, "• ")
+    #   when /^(\s{2,3}- )/ then line.gsub($1, "  ◦ ")
+    #   when /^(\s{4,})(- )/
+    #     indent_levels = $1.length / 2
+    #     indent = "  " * indent_levels
+    #     line.gsub("#{$1}#{$2}", "#{indent}▪ ")
+    #   else
+    #     indent_levels = prev_line.match(/^\s*/)[0].length / 2
+    #     indent = "  " * (indent_levels + 1)
+    #     "#{indent}#{line}"
+    #   end
+    #   prev_line = line
+    #   "  #{line}"
+    # end.join("\n")
   end
 end
